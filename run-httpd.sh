b341b628ced3e2c1a53fdcbdeeecf6dbf34a2bb8
tuxtron <32469400+tuxtron@users.noreply.github.com>
2020-03-04 09:14:51 -0300

    Add files via upload

--- run-httpd.sh
+++ run-httpd.sh
@@ -4,8 +4,6 @@
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
-
-
 echo "dec6dwbv-mobd1" >> /etc/hosts
-echo "172.0.0.1 dec6dwbv-mobd1" >> /etc/hosts
+
 exec /usr/sbin/apachectl -DFOREGROUND
